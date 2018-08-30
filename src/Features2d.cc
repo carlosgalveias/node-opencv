ed444d9edbdeef2c2b527958b2d5eb6ca3aac5bb
miguelvasques <42841820+miguelvasques@users.noreply.github.com>
2018-08-30 16:24:05 +0100

    Update Features2d.cc
    fixing syntax

--- src/Features2d.cc
+++ src/Features2d.cc
@@ -7,8 +7,6 @@
 #include <stdio.h>
 #include <limits>
 
-#ifdef HAVE_OPENCV_FEATURES2D
-
 void Features::Init(Local<Object> target) {
   Nan::HandleScope scope;
 
@@ -49,11 +47,15 @@ public:
     extractor->compute(image1, keypoints1, descriptors1);
     extractor->compute(image2, keypoints2, descriptors2);
 
+    //fix for 'Assertion failed...' error
+    //source: http://stackoverflow.com/questions/15650371/matcher-assertions-failed-error-opencv-android
     if (descriptors1.type() == descriptors2.type() && descriptors1.cols == descriptors2.cols) {
       matcher->match(descriptors1, descriptors2, matches);
-       double max_dist = 0;
+
+      double max_dist = 0;
       double min_dist = 100;
-       //-- Quick calculation of max and min distances between keypoints
+
+      //-- Quick calculation of max and min distances between keypoints
       for (int i = 0; i < descriptors1.rows; i++) {
         double dist = matches[i].distance;
         if (dist < min_dist) {
@@ -64,25 +66,29 @@ public:
         }
       }
 
-    //-- Draw only "good" matches (i.e. whose distance is less than 2*min_dist,
-    //-- or a small arbitary value ( 0.02 ) in the event that min_dist is very
-    //-- small)
-    //-- PS.- radiusMatch can also be used here.
-    std::vector<cv::DMatch> good_matches;
-    double good_matches_sum = 0.0;
+      //-- Draw only "good" matches (i.e. whose distance is less than 2*min_dist,
+      //-- or a small arbitary value ( 0.02 ) in the event that min_dist is very
+      //-- small)
+      //-- PS.- radiusMatch can also be used here.
+      std::vector<cv::DMatch> good_matches;
+      double good_matches_sum = 0.0;
 
-    for (int i = 0; i < descriptors1.rows; i++) {
-      double distance = matches[i].distance;
-      if (distance <= std::max(2 * min_dist, 0.02)) {
-        good_matches.push_back(matches[i]);
-        good_matches_sum += distance;
+      for (int i = 0; i < descriptors1.rows; i++) {
+        double distance = matches[i].distance;
+        if (distance <= std::max(2 * min_dist, 0.02)) {
+          good_matches.push_back(matches[i]);
+          good_matches_sum += distance;
+        }
       }
-    }
 
-    dissimilarity = (double) good_matches_sum / (double) good_matches.size();
-  } else {
+      dissimilarity = (double) good_matches_sum / (double) good_matches.size();
+
+    }
+    else {
       dissimilarity = std::numeric_limits<double>::quiet_NaN();
     }
+    
+  }
 
   void HandleOKCallback() {
     Nan::HandleScope scope;
@@ -90,7 +96,6 @@ public:
     Local<Value> argv[2];
 
     argv[0] = Nan::Null();
-    // argv[1] = Nan::New<Number>(dissimilarity); NEW
 
     if (dissimilarity != dissimilarity) argv[1] = Nan::Null();
     else argv[1] = Nan::New<Number>(dissimilarity);
@@ -118,5 +123,4 @@ NAN_METHOD(Features::Similarity) {
   return;
 }
 
-#endif
 #endif
